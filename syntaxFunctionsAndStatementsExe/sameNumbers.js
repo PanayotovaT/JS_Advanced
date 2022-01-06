73a8dc89ac8a331634099b300b2361444e297a8f
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-06 17:31:24 +0200

    Added a file

---
+++ syntaxFunctionsAndStatementsExe/sameNumbers.js
@@ -0,0 +1,18 @@
+function solve(number) {
+
+    const string = number.toString();
+    let isSame = true;
+    let sum = 0
+
+    for (let i = 0; i < string.length; i++) {
+        let next = string[i + 1]
+        if (string[i] !== string[i + 1] && next !== undefined) {
+            isSame = false;
+        }
+        sum += Number(string[i]);
+    }
+    return `${isSame}\n${sum}`
+
+}
+console.log(solve(2222222));
+console.log(solve(1234));
