6340a8b419ad44789cffe934236cbeb80da5b774
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-06 17:26:27 +0200

    Added an exercise

---
+++ syntaxFunctionsAndStatements/demo.js
@@ -0,0 +1,4 @@
+console.log(5 > 7 ? 4 : 10);
+console.log(Number.isNaN(Number('4.4'))); //metod, proverqva dali parametyra e NaN
+console.log(parseInt('4.5kkk'))
+console.log(parseFloat('4.5kkk')) //ignorirat texta sled chisloto
