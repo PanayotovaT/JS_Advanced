d6a476d89edfd73507449f880d97fb05573ee7fe
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-07 23:17:17 +0200

    Added an exercise

---
+++ arraysAndNestedArraysExe/arrayWithDelimiterSecondV.js
@@ -0,0 +1,17 @@
+let solve = (array, char) => {
+    
+    return array.join(char);
+}
+console.log(solve(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'));
+console.log(solve(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'));
