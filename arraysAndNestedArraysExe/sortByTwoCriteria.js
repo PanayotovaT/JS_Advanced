df9b00a13714195914b1b17f58e98b18763568de
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-08 00:12:50 +0200

    Added an exercise

---
+++ arraysAndNestedArraysExe/sortByTwoCriteria.js
@@ -0,0 +1,17 @@
+function solve(array) {
+
+    return array
+    .sort ((a, b) => a.length - b.length || a.localeCompare(b))
+    .join('\n')
+}
+console.log(solve(['alpha', 'beta', 'gamma']));
+console.log(solve(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+));
+console.log(solve(['test',
+    'Deny',
+    'omen',
+    'Default']));
