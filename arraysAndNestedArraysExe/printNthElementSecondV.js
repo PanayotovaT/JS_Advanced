790a60f763a287a5e62f6e72d2a8e1323a458583
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-08 00:12:02 +0200

    Added an exercise

---
+++ arraysAndNestedArraysExe/printNthElementSecondV.js
@@ -0,0 +1,24 @@
+const solve = (array, step) => {
+
+    let result = array.filter((x, i)=> i % step === 0)
+
+    return result;
+}
+
+console.log(solve(['5',
+    '20',
+    '31',
+    '4',
+    '20'],
+    2));
+console.log(solve(['dsa',
+    'asd',
+    'test',
+    'tset'],
+    2));
+console.log(solve(['1',
+    '2',
+    '3',
+    '4',
+    '5'],
+    6));
