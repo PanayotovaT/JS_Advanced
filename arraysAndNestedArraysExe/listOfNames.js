47287085a144ab4dbd765f0c08e724c2b879f2eb
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-08 00:10:59 +0200

    Added an exercise

---
+++ arraysAndNestedArraysExe/listOfNames.js
@@ -0,0 +1,12 @@
+function solve(array) {
+
+    let result  = array
+    .sort((a, b) => a.localeCompare(b))
+    .map((name, index) => {
+       
+       let result =`${index + 1}.${name}`;
+        return result;
+    } )
+    return result.join('\n');
+}
+console.log(solve(["John", "Bob", "Christina", "Ema"]))
