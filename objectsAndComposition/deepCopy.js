7b992e8e9f55b229b6e374e0b6b2d4297b915ff2
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-09 23:26:39 +0200

    Added a new exercise

---
+++ objectsAndComposition/deepCopy.js
@@ -0,0 +1,24 @@
+const myObj = {
+    name: 'Alex',
+    lastName: 'Panayotov',
+    sayHi: function() {
+        console.log('Hi!');
+    },
+    fullName() {
+        return `${this.name} ${this.lastName}`
+    }
+};
+
+function deepCopy(target) {
+    if(Array.isArray(target)) {
+        return target.map(deepCopy);
+    } else if(typeof target == 'object') {
+        return [...Object.entries(target)].reduce((a,[k,v]) => Object.assign(a, {[k]: deepCopy(v)}) , {});
+    } else {
+        return target;
+    }
+}
+
+const myCopy = deepCopy(myObj);
+console.log(myCopy);
+console.log(myObj == myCopy);
