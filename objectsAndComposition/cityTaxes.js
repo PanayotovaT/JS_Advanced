c9ea63cdaf9349b4682d60103e5c42cb63eb97d0
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-09 23:25:59 +0200

    Added a new exercise

---
+++ objectsAndComposition/cityTaxes.js
@@ -0,0 +1,29 @@
+function solve(name, population, treasury) {
+
+    const city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * percent / 100);
+        },
+        applyRecession(percent){
+            this.treasury -= Math.ceil(this.treasury * percent / 100);
+        }
+    };
+
+    return city;
+}
+
+const city =
+  solve('Tortuga',
+  7000,
+  15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
