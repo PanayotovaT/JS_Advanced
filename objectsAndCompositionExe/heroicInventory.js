6c915b3375e759a4abae47bde8bcf9d16f17956d
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-10 02:08:08 +0200

    Added an exercise

---
+++ objectsAndCompositionExe/heroicInventory.js
@@ -0,0 +1,15 @@
+function solve(input) {
+
+    let result = []
+    while(input.length) {
+        let hero =  input.shift();
+        let [name, level, itemsString] = hero.split(' / ');
+        level = Number(level);
+        const items = itemsString ? itemsString.split(', ') : []
+
+        result.push({name, level, items});
+    }
+
+    return JSON.stringify(result);
+}
+console.log(solve(['Jake / 1000']));
