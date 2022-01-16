4972bb28625178a61916cd97b67c67076ba29dfc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-16 21:54:13 +0200

    Added an exercise

---
+++ unitTestingAndErrorHandling/subsumUnitTesting.js
@@ -0,0 +1,32 @@
+function subSum(array, start, end) {
+    if(Array.isArray(array) == false) {
+        return NaN;
+    }
+    // if (start < 0) {
+    //     start = 0;
+    // }
+
+    if(end > array.length - 1) {
+        end = array.length -1;
+    }
+    return array.slice(start, end + 1).reduce((a, c) => a + Number(c),0);
+}
+
+function test(id, actual, expected) {
+
+    if(actual == expected) {
+        console.log(`Test ${id} Pass`);
+    } else {
+        console.error(`Test ${id} Failed!`)
+    }
+}
+
+test(1, subSum([10, 20, 30, 40, 50, 60], 3, 300), 150 );
+test(2, subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1).toFixed(2), 3.3 );
+test(3, subSum([], 1, 2), 0 );
+
+/*console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300))
+console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1))
+console.log(subSum([10, 'twenty', 30, 40], 0, 2))
+console.log(subSum([], 1, 2))
+console.log(subSum('text', 0, 2))*/
