81a7bf59ec2f007387e2df1f5fa6069aace4e9c2
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-17 21:51:53 +0200

    Added an exercise

---
+++ unitTestingAndErrorHandlingExe/dynamicValidation/app.js
@@ -0,0 +1,14 @@
+function validate() {
+    const input = document.getElementById('email');
+    const pattern = /[a-z]+@[a-z]+.[a-z]+/gm;
+    input.addEventListener('change', (e) => {
+        if(!pattern.test(input.value)) {
+
+            input.classList.add('error')
+
+        } else {
+            input.removeAttribute('class');
+        }
+
+    });
+};
