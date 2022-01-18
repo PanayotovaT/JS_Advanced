7dab1cb04aa22fe8a346e08bea9e9b79c2afebfc
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-18 23:41:39 +0200

    Added an exercise

---
+++ classes/demo-browser.js
@@ -0,0 +1,61 @@
+// temp0
+// <a class="view-submission-button btn btn-default pull-right" href="/Contests/Submissions/View/19509300" target="_blank">
+
+// const mySpan = document.createElement('span');
+// undefined
+// mySpan.textContent = 'replaced';
+// "replaced"
+// temp0.replaceWith(mySpan);
+// undefined
+// temp1
+// <div class="k-toolbar k-grid-toolbar k-grid-top" style="border-width: 1px 0px;">
+
+// const button =document.createElement('button');
+// undefined
+// button.textContent ='Submissions';
+// "Submissions"
+// temp1.replaceWith(button);
+// undefined
+// temp2
+// <span class="k-link">
+
+// templ2.classList
+// Uncaught ReferenceError: templ2 is not defined
+//     <anonymous> debugger eval code:1
+// debugger eval code:1:1
+// temp2.classList;
+// DOMTokenList(1)
+// ​
+// 0: "k-link"
+// ​
+// length: 1
+// ​
+// value: "k-link"
+// ​
+// <prototype>: DOMTokenListPrototype { item: item(), contains: contains(), add: add(), … }
+
+// temp2.classList.add('myClass');
+// undefined
+// temp2;
+// <span class="k-link myClass">
+
+// temp2.classList
+// DOMTokenList [ "k-link", "myClass" ];
+
+// temp2.classList.remove('myClass');
+
+// temp3
+// <textarea style="position: absolute; bott…rrentcolor none medium;" autocorrect="off" autocapitalize="none" spellcheck="false" tabindex="0" wrap="off">
+
+// temp3.spellcheck;
+// false
+// temp3.getAttribute('spellcheck');
+// "false"
+// temp3.setAttribute('spellcheck', 'true');
+// undefined
+// temp3.getAttribute('spellcheck');
+// "true"
+// temp3.removeAttribute('spellcheck');
+// undefined
+
+// hasAttribute() // => proverqva dali ima attribute;
