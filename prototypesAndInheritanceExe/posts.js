63473437849705983bc482061d7e9068e10f92bf
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-21 23:40:55 +0200

    Added an exercise

---
+++ prototypesAndInheritanceExe/posts.js
@@ -0,0 +1,85 @@
+function solve() {
+    
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+           return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+    class SocialMediaPost extends Post{
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+        
+        addComment(comment) {
+            this.comments.push(comment)
+        }
+        toString() {
+            let result = super.toString();
+            let rating = `Rating: ${this.likes - this.dislikes}`;
+            let comments = this.comments.length > 0 
+            ? `Comments:\n${this.comments.map(comment => ` * ${comment}`).join('\n')}`
+            : '';
+
+            return [result, rating, comments].join('\n').trim();
+        }
+    }
+
+// "Post: {postTitle}"
+// "Content: {postContent}"
+// "Rating: {postLikes - postDislikes}"
+// "Comments:"
+// " * {comment1}"
+// " * {comment2}"
+// . . .
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`
+        }
+    }
+    
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost
+    }
+}
+let f = solve();
+let post = new f.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new f.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
